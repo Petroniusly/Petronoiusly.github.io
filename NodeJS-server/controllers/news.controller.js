a4af8c331f7f5ffe2b2bc136f29f13e8202ca2fc
Petroniusly <blinkovpag@gmail.com>
2019-01-21 23:37:38 +0300

    Task7 (NodeJS) : level 4-5 completed

--- NodeJS-server/controllers/news.controller.js
+++ NodeJS-server/controllers/news.controller.js
@@ -1,57 +1,75 @@
 const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
+const ValidationError = require('../errors/ValidationError.component')
 
-function getArticleById(req, res) {
+function getArticleById(req, res, next) {
   fs.readFile('./mockData.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) next(err);
   
     let newData = JSON.parse(data);
     if (_.isUndefined(newData[req.params.id])) {
-      throw err;
+      next(new ValidationError(`article with id = ${req.params.id} is not found`));
     } else {
       res.json(newData[req.params.id]);
     }
   });
 }
 
-function getAllNews(req, res) {
+function getNewsBlock(req,res,next) {
   fs.readFile('./mockData.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) next(err);
 
     res.json(JSON.parse(data));
   })
 }
 
-function putOneArticleById(req, res) {
+function putOneArticleById(req,res,next) {
   fs.readFile('./mockData.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) next(err);
     
     let newData = JSON.parse(data);
     if (_.isUndefined(newData[req.params.id])) {
       newData[req.params.id] = req.body;
       fs.writeFile('./mockData.json', JSON.stringify(newData, null, 2), (err, data) => {
-        if (err) throw err;
+        if (err) next(err);
     
-        res.status(200).send();
+        res.status(201).send();
       });
     } else {
-      throw err;
+      next(new ValidationError(`article with id = ${req.params.id} is already exist`));
     }
   });
 }
 
-function updateArticleById(req, res) {
+function putNewsBlock(req,res,next) {
   fs.readFile('./mockData.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) next(err);
+    
+    let newData = JSON.parse(data);
+    if (_.isUndefined(newData)) {
+      fs.writeFile('./mockData.json', JSON.stringify(newData, null, 2), (err, data) => {
+        if (err) next(err);
+    
+        res.status(201).send();
+      });
+    } else {
+      next(new ValidationError(`news block is already exist`));
+    }
+  });
+}
+
+function updateArticleById(req,res,next) {
+  fs.readFile('./mockData.json', 'utf8', (err, data) => {
+    if (err) next(err);
     
     let newData = JSON.parse(data);
     if (_.isUndefined(newData[req.params.id])) {
-      throw err;
+      next(new ValidationError(`article with id = ${req.params.id} is not found to be updated`));
     } else {
       newData[req.params.id] = req.body;
       fs.writeFile('./mockData.json', JSON.stringify(newData, null, 2), (err, data) => {
-        if (err) throw err;
+        if (err) next(err);
     
         res.status(200).send();
       });
@@ -59,25 +77,25 @@ function updateArticleById(req, res) {
   });
 }
 
-function updateNewsBlock(req, res) {
+function updateNewsBlock(req,res,next) {
   fs.writeFile('./mockData.json', JSON.stringify(req.body, null, 2), (err, data) => {
-    if (err) throw err;
+    if (err) next(err);
     
     res.status(200).send();
   });
 }
 
-function deleteArticleById(req, res) {
+function deleteArticleById(req,res,next) {
   fs.readFile('./mockData.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) next(err);
     
     let parsedData = JSON.parse(data);
     if (_.isUndefined(parsedData[req.params.id])) {
-      throw err;
+      next(new ValidationError(`article with id = ${req.params.id} is not exist`));
     } else {
       let newData = _.omit(parsedData, [req.params.id]);
       fs.writeFile('./mockData.json', JSON.stringify(newData, null, 2), (err, data) => {
-        if (err) throw err;
+        if (err) next(err);
         
         res.status(200).send();
       });
@@ -85,9 +103,9 @@ function deleteArticleById(req, res) {
   });
 }
 
-function deleteNewsBlock(req, res) {
+function deleteNewsBlock(req,res,next) {
   fs.writeFile('./mockData.json', JSON.stringify({}), (err, data) => {
-    if (err) throw err;
+    if (err) next(err);
     
     res.status(200).send();
   });
@@ -96,9 +114,11 @@ function deleteNewsBlock(req, res) {
 // Read
 module.exports.getArticleById = getArticleById;
 // Read all news
-module.exports.getAllNews = getAllNews;
+module.exports.getNewsBlock = getNewsBlock;
 // Create
 module.exports.putOneArticleById = putOneArticleById;
+// Create
+module.exports.putNewsBlock = putNewsBlock;
 // Update
 module.exports.updateArticleById = updateArticleById;
 // Update all news
--- NodeJS-server/routes/routes.js
+++ NodeJS-server/routes/routes.js
@@ -1,22 +1,25 @@
-const newsController = require('../controllers/news.controller');
 const express = require('express');
 const _ = require('lodash');
+const newsController = require('../controllers/news.controller');
+const logger = require('../handlers/logger');
 
 const router = express.Router();
 
-// Read all news
-router.get('/news', newsController.getAllNews);
-// Read
-router.get('/news/:id', newsController.getArticleById);
-// Create
-router.put('/news/:id', newsController.putOneArticleById);
-// Update
-router.post('/news', newsController.updateNewsBlock);
-// // Delete all news
-router.post('/news/:id', newsController.updateArticleById);
-// // Update all news
-router.delete('/news', newsController.deleteNewsBlock);
-// // Delete
-router.delete('/news/:id', newsController.deleteArticleById);
+// "C: create, R: read, U: update, and D: delete"
+// "create - put, read - get, update - post, delete - delete"
+router.route('/*')
+  .all(logger.logConnect);
+
+router.route('/news')
+  .get(newsController.getNewsBlock)
+  .post(newsController.updateNewsBlock)
+  .put(newsController.putNewsBlock)
+  .delete(newsController.deleteNewsBlock);
+
+router.route('/news/:id')
+  .get(newsController.getArticleById)
+  .post(newsController.updateArticleById)
+  .put(newsController.putOneArticleById)
+  .delete(newsController.deleteArticleById);
 
 module.exports = router;
